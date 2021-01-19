9399fe301334a768aa72f4f52c9f903a6042e2bf
ChengTheRipper <wuzhuocheng256@gmail.com>
2021-01-19 10:37:44 +0800

    添加了属性表以优化项目配置过程
    1.新建了两个属性表
    2. 设置项目包含路径等为继承

--- src/StreamProcessor/HeightCal.cpp
+++ src/StreamProcessor/HeightCal.cpp
@@ -29,94 +29,94 @@ size_t HeightCal::NumberOfTrackedJoints(const std::vector<astra::Joint> joint_li
 	return num_of_tracked;
 }
 
-double HeightCal::GetBodyHeight()
-{
-	const double HEAD_DIVERGENCE = 0.1;
-
-	auto head = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::Head)
-				return cur_joint;
-		}
-	);
-	auto neck = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::ShoulderSpine)
-				return cur_joint;
-		}
-	);
-	auto spine = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::MidSpine)
-				return cur_joint;
-		}
-	);
-	auto waist = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::BaseSpine)
-				return cur_joint;
-		}
-	);
-	auto hip_left = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::LeftHip)
-				return cur_joint;
-		}
-	);
-	auto hip_right = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::RightHip)
-				return cur_joint;
-		}
-	);
-	auto knee_left = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::LeftKnee)
-				return cur_joint;
-		}
-	);
-	auto knee_right = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::RightKnee)
-				return cur_joint;
-		}
-	);
-	auto foot_left = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::LeftFoot)
-				return cur_joint;
-		}
-	);
-	auto foot_right = find_if(joint_list_.begin(), joint_list_.end(),
-		[](Joint& cur_joint)
-		{
-			if (cur_joint.type() == JointType::RightFoot)
-				return cur_joint;
-		}
-	);
-
-	// Find which leg is tracked more accurately.
-	vector<Joint> left_leg = { *hip_left, *knee_left, *foot_left };
-	vector<Joint> right_leg = { *hip_right, *knee_right, *foot_right };
-	int leg_left_tracked_joints = NumberOfTrackedJoints(left_leg);
-	int leg_right_tracked_joints = NumberOfTrackedJoints(right_leg);
-
-	if (leg_left_tracked_joints == 0 && leg_right_tracked_joints == 0)
-		return -1.0;
-
-	double leg_length = leg_left_tracked_joints > leg_right_tracked_joints ?
-		BoneLength(*hip_left, *knee_left) + BoneLength(*hip_left, *knee_left)
-		: BoneLength(*hip_right, *knee_right) + BoneLength(*hip_right, *knee_right);
-
-	double body_length = BoneLength(*head, *neck) + BoneLength(*neck, *spine) + BoneLength(*spine, *waist);
-	return body_length + leg_length + HEAD_DIVERGENCE;
-}
+//double HeightCal::GetBodyHeight()
+//{
+//	const double HEAD_DIVERGENCE = 0.1;
+//
+//	auto head = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::Head)
+//				return cur_joint;
+//		}
+//	);
+//	auto neck = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::ShoulderSpine)
+//				return cur_joint;
+//		}
+//	);
+//	auto spine = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::MidSpine)
+//				return cur_joint;
+//		}
+//	);
+//	auto waist = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::BaseSpine)
+//				return cur_joint;
+//		}
+//	);
+//	auto hip_left = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::LeftHip)
+//				return cur_joint;
+//		}
+//	);
+//	auto hip_right = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::RightHip)
+//				return cur_joint;
+//		}
+//	);
+//	auto knee_left = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::LeftKnee)
+//				return cur_joint;
+//		}
+//	);
+//	auto knee_right = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::RightKnee)
+//				return cur_joint;
+//		}
+//	);
+//	auto foot_left = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::LeftFoot)
+//				return cur_joint;
+//		}
+//	);
+//	auto foot_right = find_if(joint_list_.begin(), joint_list_.end(),
+//		[](Joint& cur_joint)
+//		{
+//			if (cur_joint.type() == JointType::RightFoot)
+//				return cur_joint;
+//		}
+//	);
+//
+//	// Find which leg is tracked more accurately.
+//	vector<Joint> left_leg = { *hip_left, *knee_left, *foot_left };
+//	vector<Joint> right_leg = { *hip_right, *knee_right, *foot_right };
+//	int leg_left_tracked_joints = NumberOfTrackedJoints(left_leg);
+//	int leg_right_tracked_joints = NumberOfTrackedJoints(right_leg);
+//
+//	if (leg_left_tracked_joints == 0 && leg_right_tracked_joints == 0)
+//		return -1.0;
+//
+//	double leg_length = leg_left_tracked_joints > leg_right_tracked_joints ?
+//		BoneLength(*hip_left, *knee_left) + BoneLength(*hip_left, *knee_left)
+//		: BoneLength(*hip_right, *knee_right) + BoneLength(*hip_right, *knee_right);
+//
+//	double body_length = BoneLength(*head, *neck) + BoneLength(*neck, *spine) + BoneLength(*spine, *waist);
+//	return body_length + leg_length + HEAD_DIVERGENCE;
+//}
