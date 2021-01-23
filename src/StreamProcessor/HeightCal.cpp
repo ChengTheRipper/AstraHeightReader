5f81fa1748faade8f5fd46a6bb262a651df1c558
ChengTheRipper <wuzhuocheng256@gmail.com>
2021-01-23 22:35:11 +0800

    基本功能测试完毕，身高仍有误差，测量距离过远，需要进行修正

--- src/StreamProcessor/HeightCal.cpp
+++ src/StreamProcessor/HeightCal.cpp
@@ -29,94 +29,109 @@ size_t HeightCal::NumberOfTrackedJoints(const std::vector<astra::Joint> joint_li
 	return num_of_tracked;
 }
 
-//double HeightCal::GetBodyHeight()
-//{
-//	const double HEAD_DIVERGENCE = 0.1;
-//
-//	auto head = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::Head)
-//				return cur_joint;
-//		}
-//	);
-//	auto neck = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::ShoulderSpine)
-//				return cur_joint;
-//		}
-//	);
-//	auto spine = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::MidSpine)
-//				return cur_joint;
-//		}
-//	);
-//	auto waist = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::BaseSpine)
-//				return cur_joint;
-//		}
-//	);
-//	auto hip_left = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::LeftHip)
-//				return cur_joint;
-//		}
-//	);
-//	auto hip_right = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::RightHip)
-//				return cur_joint;
-//		}
-//	);
-//	auto knee_left = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::LeftKnee)
-//				return cur_joint;
-//		}
-//	);
-//	auto knee_right = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::RightKnee)
-//				return cur_joint;
-//		}
-//	);
-//	auto foot_left = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::LeftFoot)
-//				return cur_joint;
-//		}
-//	);
-//	auto foot_right = find_if(joint_list_.begin(), joint_list_.end(),
-//		[](Joint& cur_joint)
-//		{
-//			if (cur_joint.type() == JointType::RightFoot)
-//				return cur_joint;
-//		}
-//	);
-//
-//	// Find which leg is tracked more accurately.
-//	vector<Joint> left_leg = { *hip_left, *knee_left, *foot_left };
-//	vector<Joint> right_leg = { *hip_right, *knee_right, *foot_right };
-//	int leg_left_tracked_joints = NumberOfTrackedJoints(left_leg);
-//	int leg_right_tracked_joints = NumberOfTrackedJoints(right_leg);
-//
-//	if (leg_left_tracked_joints == 0 && leg_right_tracked_joints == 0)
-//		return -1.0;
-//
-//	double leg_length = leg_left_tracked_joints > leg_right_tracked_joints ?
-//		BoneLength(*hip_left, *knee_left) + BoneLength(*hip_left, *knee_left)
-//		: BoneLength(*hip_right, *knee_right) + BoneLength(*hip_right, *knee_right);
-//
-//	double body_length = BoneLength(*head, *neck) + BoneLength(*neck, *spine) + BoneLength(*spine, *waist);
-//	return body_length + leg_length + HEAD_DIVERGENCE;
-//}
+double HeightCal::GetBodyHeight()
+{
+	const double HEAD_DIVERGENCE = 0.1;
+	astra::Joint head, neck, shoulder_spine, spine, waist, hip_left, hip_right,
+		knee_left, knee_right, foot_left, foot_right;
+	for (const auto& joint : joint_list_)
+	{
+		JointType cur_type = joint.type();
+		switch (cur_type)
+		{
+		case astra::JointType::Head:
+			head = joint;
+			if (head.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::ShoulderSpine:
+			shoulder_spine = joint;
+			if (shoulder_spine.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::LeftShoulder:
+			break;
+		case astra::JointType::LeftElbow:
+			break;
+		case astra::JointType::LeftHand:
+			break;
+		case astra::JointType::RightShoulder:
+			break;
+		case astra::JointType::RightElbow:
+			break;
+		case astra::JointType::RightHand:
+			break;
+		case astra::JointType::MidSpine:
+			spine = joint;
+			if (spine.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::BaseSpine:
+			waist = joint;
+			if (spine.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::LeftHip:
+			hip_left = joint;
+			if (hip_left.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::LeftKnee:
+			knee_left = joint;
+			if (knee_left.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::LeftFoot:
+			foot_left = joint;
+			if (foot_left.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::RightHip:
+			hip_right = joint;
+			if (hip_right.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::RightKnee:
+			knee_right = joint;
+			if (knee_right.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::RightFoot:
+			foot_right = joint;
+			if (foot_right.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::LeftWrist:
+			break;
+		case astra::JointType::RightWrist:
+			break;
+		case astra::JointType::Neck:
+			neck = joint;
+			if (neck.status() == JointStatus::NotTracked)
+				return -1.0;
+			break;
+		case astra::JointType::Unknown:
+			break;
+		default:
+			break;
+		}
+	}
+
+
+
+	// Find which leg is tracked more accurately.
+	vector<Joint> left_leg = { hip_left,  knee_left,  foot_left };
+	vector<Joint> right_leg = { hip_right, knee_right, foot_right };
+	int leg_left_tracked_joints = NumberOfTrackedJoints(left_leg);
+	int leg_right_tracked_joints = NumberOfTrackedJoints(right_leg);
+
+	if (leg_left_tracked_joints == 0 && leg_right_tracked_joints == 0)
+		return -1.0;
+
+	double leg_length = leg_left_tracked_joints > leg_right_tracked_joints ?
+		BoneLength(hip_left, knee_left) + BoneLength(knee_left, foot_left)
+		: BoneLength(hip_right, knee_right) + BoneLength(knee_right, foot_right);
+
+	double body_length = 2 * BoneLength(head, neck) + BoneLength(neck, spine) + BoneLength(spine, waist) + BoneLength(waist, hip_left);
+	return body_length + leg_length + HEAD_DIVERGENCE;
+}
